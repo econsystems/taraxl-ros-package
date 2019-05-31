d1af0a751a1e14b310339f8148e83b10ceebccfb
Vishnu <prashanth.r@e-consystems.com>
2019-05-31 14:17:03 +0530

    ROS Wrapper for v3.0.1

---
+++ taraxl_ros_package/include/TaraXLPointcloud.h
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2018, e-con Systems.
+//
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS.
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT/INDIRECT DAMAGES HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////
+/**********************************************************************
+TaraXLPointcloud.h : implementations of pointcloud and other 3D options.
+**********************************************************************/
+#ifndef TARAXL_POINTCLOUD_H_
+#define TARAXL_POINTCLOUD_H_
+
+#include "TaraXLCam.h"
+#include "pcl/common/common_headers.h"
+
+namespace TaraXLSDK
+{
+
+    typedef pcl::PointCloud<pcl::PointXYZRGB> Points;
+
+    class TaraXLPointcloud
+    {
+    public:
+        TaraXLPointcloud(TaraXLCam &camera);
+
+        //Sets the quality of the pointcloud that is to be rendered.
+        TARAXL_STATUS_CODE setPointcloudQuality(TARAXL_POINTCLOUD_QUALITY pointcloudQuality);
+
+        //Saves the pointcloud at that particular frame using the given file name
+        TARAXL_STATUS_CODE savePoints(TARAXL_POINTCLOUD_FORMAT pointcloudFormat, std::string filename);
+
+        //Gets the pointcloud in the specified format.
+        TARAXL_STATUS_CODE getPoints(Points::Ptr currentCloud);
+
+    };
+}
+#endif  /* TARAXL_POINTCLOUD_H_ */
---
+++ taraxl_ros_package/include/TaraXLPoseTracking.h
@@ -0,0 +1,52 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2018, e-con Systems.
+//
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS.
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT/INDIRECT DAMAGES HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////
+/**********************************************************************
+TaraXLPoseTracking.h : Pose tracking APIs for IMUs
+**********************************************************************/
+#ifndef TARAXL_POSE_TRACKING_H_
+#define TARAXL_POSE_TRACKING_H_
+
+#include "TaraXLEnums.h"
+
+namespace TaraXLSDK
+{
+
+    //Struct that contains the IMU data.
+    struct TaraXLIMUData
+    {
+        Vector3 angularVelocity;
+        Vector3 linearAcceleration;
+        Vector3 getInclination();
+        TaraXLIMUData();
+    };
+
+
+    class TaraXLPoseTracking
+    {
+    public:
+        TaraXLPoseTracking(TaraXLCam &camera);
+        ~TaraXLPoseTracking();
+
+        //Sets the output frequency
+        TARAXL_STATUS_CODE setIMUOutputFrequency(TARAXL_IMU_OUTPUT_FREQUENCY frequency);
+
+        //Gets the output frequency
+        TARAXL_STATUS_CODE getIMUOutputFrequency(TARAXL_IMU_OUTPUT_FREQUENCY &frequency);
+
+        //Gets the IMU data.
+        TARAXL_STATUS_CODE getIMUData(TaraXLIMUData &data);
+    };
+}
+#endif  /* TARAXL_POSE_TRACKING_H_ */
